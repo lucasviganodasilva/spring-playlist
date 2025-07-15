dfdcb3c1ff29fedda18d59bff35cb1560158eb84
Lucas Viganó da Silva <lucasviganodasilva@gmail.com>
2025-07-14 21:45:42 -0300

    Finalização do Controller de Playlist

--- playlist/src/main/java/spring_api/playlist/controller/PlaylistController.java
+++ playlist/src/main/java/spring_api/playlist/controller/PlaylistController.java
@@ -2,10 +2,7 @@ package spring_api.playlist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring_api.playlist.model.Playlist;
 import spring_api.playlist.service.PlaylistService;
 
@@ -23,13 +20,36 @@ public class PlaylistController {
     public ResponseEntity<?> create(@RequestBody Playlist playlist) {
 
         if(playlist.getName() == null) {
-            return ResponseEntity.badRequest().body("O nome da playlist é obrigatório");
+            return ResponseEntity.status(400).body("O nome da playlist é obrigatório");
         }
 
         playlistService.createPlaylist(playlist);
         URI url = URI.create("/lists" + playlist.getName());
         return ResponseEntity.ok(url);
+    }
 
+    @GetMapping
+    public List<Playlist> listAllPlaylists() {
+        return playlistService.listPlaylists();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<?> showListDescription(@PathVariable String name) {
+        try {
+            return ResponseEntity.ok(playlistService.findByName(name));
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body("Playlist não encontrada");
+        }
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<?> deletePlaylist(@PathVariable String name) {
+        try {
+            playlistService.removeByName(name);
+            return ResponseEntity.status(204).body("Playlist excluída com sucesso");
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(404).body("Playlist não encontrada");
+        }
     }
 
 
