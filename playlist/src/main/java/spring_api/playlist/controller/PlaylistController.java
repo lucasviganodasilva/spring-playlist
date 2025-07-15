159a2f4a5faddee170e73a7857706e63c7d82ee4
Lucas Viganó da Silva <lucasviganodasilva@gmail.com>
2025-07-14 21:28:55 -0300

    Criação do método de Post para o controller da Playlist

--- playlist/src/main/java/spring_api/playlist/controller/PlaylistController.java
+++ playlist/src/main/java/spring_api/playlist/controller/PlaylistController.java
@@ -1,14 +1,36 @@
 package spring_api.playlist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import spring_api.playlist.model.Playlist;
 import spring_api.playlist.service.PlaylistService;
 
+import java.net.URI;
+import java.util.List;
+
 @RestController
+@RequestMapping("/lists")
 public class PlaylistController {
 
     @Autowired
     private PlaylistService playlistService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Playlist playlist) {
+
+        if(playlist.getName() == null) {
+            return ResponseEntity.badRequest().body("O nome da playlist é obrigatório");
+        }
+
+        playlistService.createPlaylist(playlist);
+        URI url = URI.create("/lists" + playlist.getName());
+        return ResponseEntity.ok(url);
+
+    }
 
 
 }
