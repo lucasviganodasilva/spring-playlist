446fe240f8da7d15235227eed38db1cf21d63079
Lucas Viganó da Silva <lucasviganodasilva@gmail.com>
2025-07-16 19:06:56 -0300

    Finalização da classe JwtService

--- playlist/src/main/java/spring_api/playlist/security/JwtService.java
+++ playlist/src/main/java/spring_api/playlist/security/JwtService.java
@@ -2,10 +2,14 @@ package spring_api.playlist.security;
 
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @Service
 public class JwtService {
@@ -21,8 +25,18 @@ public class JwtService {
 
         Long expiry = 3600L;
 
-        String scopes = "";
+        String scopes = authentication.getAuthorities()
+                .stream().map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(" "));
 
-        return scopes;
+        var claims = JwtClaimsSet.builder()
+                .issuer("spring-security-jwt")
+                .issuedAt(now)
+                .expiresAt(now.plusSeconds(expiry))
+                .subject(authentication.getName())
+                .claim("scope", scopes)
+                .build();
+
+        return encoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
     }
 }
