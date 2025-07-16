d2edad9d04076b626acc646877e0299a21e17ba9
Lucas Viganó da Silva <lucasviganodasilva@gmail.com>
2025-07-16 19:35:12 -0300

    Finalização das configurações das classes AuthController e do AuthService

--- playlist/src/main/java/spring_api/playlist/security/AuthController.java
+++ playlist/src/main/java/spring_api/playlist/security/AuthController.java
@@ -1,18 +1,21 @@
 package spring_api.playlist.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
-    @Autowired
     private AuthService authService;
 
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("auth")
-    public String authentication() {
-        return authService.authentication();
+    public String authentication(Authentication authentication) {
+        return authService.authentication(authentication);
     }
 
 }
--- playlist/src/main/java/spring_api/playlist/security/AuthService.java
+++ playlist/src/main/java/spring_api/playlist/security/AuthService.java
@@ -1,12 +1,20 @@
 package spring_api.playlist.security;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
-    public String authentication() {
-        return "token";
+
+    private JwtService jwtService;
+
+    public AuthService(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    public String authentication(Authentication authentication) {
+        return jwtService.tokenGenerator(authentication);
     }
 
 }
